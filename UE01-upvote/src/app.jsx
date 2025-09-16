b06402ca23a49720c1c4e45962d27fbc38e4c13d
SiriGlück <siriluck.rattanapong@gmail.com>
2025-09-16 17:37:24 +0200

    Tag2: Exercise (UE01)

---
+++ UE01-upvote/src/app.jsx
@@ -0,0 +1,8 @@
+import Posts from "./components/posts";
+import "./styles.css";
+
+export default function App() {
+  return <div>
+    <Posts />
+  </div>;
+}
---
+++ UE01-upvote/src/components/posts.jsx
@@ -0,0 +1,119 @@
+export default function Posts() {
+  return (<div>
+    <div className="section">
+      <article className="media">
+        <figure className="media-left">
+          <img className="image is-64x64" src="/img/submissions/image-yellow.png" />
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>
+                <a href="#" className="has-text-info">Yellow Pail</a>
+                <span className="tag is-small">#1</span>
+              </strong>
+              <br />
+              On-demand sand castle construction expertise.
+              <br />
+              <small className="is-size-7">
+                Submitted by:
+                <img className="image is-24x24" src="/img/avatars/daniel.jpg" />
+              </small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-up"></i>
+            <strong className="has-text-info">10</strong>
+          </span>
+        </div>
+      </article>
+      <article className="media">
+        <figure className="media-left">
+          <img className="image is-64x64" src="/img/submissions/image-rose.png" />
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>
+                <a href="#" className="has-text-info">Supermajority: The Fantasy Congress League</a>
+                <span className="tag is-small">#2</span>
+              </strong>
+              <br />
+              Earn points when your favorite politicians pass legislation.
+              <br />
+              <small className="is-size-7">
+                Submitted by:
+                <img className="image is-24x24" src="/img/avatars/kristy.png" />
+              </small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-up"></i>
+            <strong className="has-text-info">11</strong>
+          </span>
+        </div>
+      </article>
+      <article className="media">
+        <figure className="media-left">
+          <img className="image is-64x64" src="/img/submissions/image-steel.png" />
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>
+                <a href="#" className="has-text-info">Tinfoild: Tailored tinfoil hats</a>
+                <span className="tag is-small">#3</span>
+              </strong>
+              <br />
+              We have your measurements and shipping address.
+              <br />
+              <small className="is-size-7">
+                Submitted by:
+                <img className="image is-24x24" src="/img/avatars/veronika.jpg" />
+              </small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-up"></i>
+            <strong className="has-text-info">17</strong>
+          </span>
+        </div>
+      </article>
+      <article className="media">
+        <figure className="media-left">
+          <img className="image is-64x64" src="/img/submissions/image-aqua.png" />
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>
+                <a href="#" className="has-text-info">Haught or Naught</a>
+                <span className="tag is-small">#4</span>
+              </strong>
+              <br />
+              High-minded or absent-minded? You decide.
+              <br />
+              <small className="is-size-7">
+                Submitted by:
+                <img className="image is-24x24" src="/img/avatars/molly.png" />
+              </small>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <span className="icon is-small">
+            <i className="fa fa-chevron-up"></i>
+            <strong className="has-text-info">9</strong>
+          </span>
+        </div>
+      </article>
+    </div>
+  </div>
+  );
+}
