25d149ab65bde4bd5c25c0a3af9347c85c6dab14
Siriluck Rattanapong <siriluck.rattanapong@gmail.com>
2025-09-17 08:31:20 +0200

    Tag3: Exercise (UE02-template)

--- UE01-upvote/src/app.jsx
+++ UE01-upvote/src/app.jsx
@@ -1,3 +1,10 @@
+// app.jsx
+// React Komponent, die im ersten Schnitt das komplette HTML
+// der index.html enthält (unterhalb #app Div-Tags natürlich)
+// und in der main.js als Hauptkomponente eine React-Anwendung
+// gestartet wird.
+// Die Komponente returned also die Zeilen 13-128 aus der index.html
+
 import Posts from "./components/posts";
 import "./styles.css";
 
---
+++ UE02-todos/README.md
@@ -0,0 +1,14 @@
+# Erstelle eine React Anwendung, die diese UI darstellt
+
+## Aufbau der App-Komponente
+
+```jsx
+  <div>
+    <h1>todos</h1>
+    <main>
+      <TodosInput />
+      <TodosList />
+      <TodosStats />
+    </main>
+  </div>
+```
